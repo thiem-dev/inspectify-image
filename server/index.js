e0143f45c024c2d419330c58b88bce3455a9f9b8
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-14 22:58:52 -0500

    basic server setup. works fine. non-vercel

--- server/index.js
+++ server/index.js
@@ -10,20 +10,34 @@ const apiPort = process.env.PORT || 3000;
 const app = express();
 
 const pool = new Pool({
-    connectionString: process.env.DB_URL
+    connectionString: process.env.CONNECTION_STRING
 })
 
 //  ------------------------------------------------------------ MIDDLEWARE
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.static('public'))
-app.use('/', (req, res) =>{
-    res.send(`Server is running`)
-})
+app.use(express.static('./public'))
+// app.use('/', (req, res) =>{
+//     res.send(`Server is running`)
+// })
 
-app.listen(apiPort, console.log(`Server started on PORT localhost:${apiPort}`))
+app.listen(apiPort, console.log(`Server started on PORT ${apiPort}`))
 
 //  ------------------------------------------------------------ DB API ROUTES
 
-
+app.get('/api/history', async (req, res) => {
+    
+    try{
+        const result = await pool.query(
+            `SELECT * FROM history`
+        );
+        if(result.rows.length === 0){
+            return res.status(400).send(`no rows in history`);
+        }
+        res.status(201).send(result.rows);
+    } catch (error){
+        console.log(error)
+        res.status(400).json(error)
+    }
+});
