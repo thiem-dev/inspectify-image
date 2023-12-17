2cb0e3faf7b535b3eb2893ea9f4934b034c0a1bb
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-17 11:22:41 -0500

    logic for items complete

--- client/src/components/MainContent.jsx
+++ client/src/components/MainContent.jsx
@@ -1,10 +1,15 @@
 //TODO add onchange and onclick to input and buttons
 // map through class results and identify function
 
-const MainContent = ({imageURL, imageRef, results, identify}) => {
+import ClassResults from "./ClassResults"
 
+const MainContent = ({imageURL, imageRef, results, identify, textInputRef}) => {
+
+
+    // console.log('results',results)
     const classResults = results.map((item) => (
         //TODO results component
+        
     ))
 
     return (
@@ -13,11 +18,12 @@ const MainContent = ({imageURL, imageRef, results, identify}) => {
             <div className="inputHolder">
                 {/* <input type='file' accept='image/*' capture='camera' className='uploadInput hide'/> */}
                 <button className='uploadImage'>Upload Image</button>
-                <input type="text" placeholder='Paste imge URL' />
+                <input type="text" placeholder='Paste imge URL' ref={textInputRef} />
+                {imageURL && <button className='button' onClick={identify}>Identify Image</button>}
             </div>
             <div className="resultsHolders">
                 <h1>Results Holder</h1>
-
+                <ClassResults results={results}/>
             </div>
             <div className="Imageholder px-6">
                 {imageURL && <img src={imageURL} alt="Upload Preview" crossOrigin="anonymous" ref={imageRef} />}
